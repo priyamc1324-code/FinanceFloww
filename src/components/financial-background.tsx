7f372ef52ed5205f3ea25e77d44cf00d076a8516
priyam2389 <priyamc1324@gmail.com>
2025-12-27 09:58:45 +0000

    again, there is no change, also change the colour from dark blue to off

--- src/components/financial-background.tsx
+++ src/components/financial-background.tsx
@@ -112,7 +112,7 @@ const FinancialBackground = () => {
   return (
     <canvas
       ref={canvasRef}
-      className="absolute top-0 left-0 w-full h-full -z-10"
+      className="absolute top-0 left-0 w-full h-full -z-10 bg-background"
     />
   );
 };
